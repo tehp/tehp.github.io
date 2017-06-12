c12c9f94b8ebce267d815ac3ae9f59317fc818e4
mac craig <maccraig98@gmail.com>
2017-06-12 16:12:34 -0700

    Add pay repository as submodule

--- shamlo/js/grid.js
+++ shamlo/js/grid.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-    var feed = new Instafeed({
-        get: 'user',
-        userId: '467087330',
-        accessToken: '467087330.1677ed0.ae54062485a64d0888f58249c3e8b2bf',
-        resolution: 'standard_resolution',
-        limit: '18',
-        //tagName: 'awesome',   (switch from user to tagged)
-        template: '<div class="grid-item-img"><a href="{{link}}"><img src="{{image}}"/></a></div>'
+  var feed = new Instafeed({
+    get: 'user',
+    userId: '467087330',
+    accessToken: '467087330.1677ed0.ae54062485a64d0888f58249c3e8b2bf',
+    resolution: 'standard_resolution',
+    limit: '18',
+    //tagName: 'awesome',   (switch from user to tagged)
+    template: '<div class="hidden-sm hidden-md hidden-xs col-xs-3"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div><div class="hidden-sm hidden-lg hidden-xs col-xs-4"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div><div class="hidden-lg hidden-md hidden-xs col-xs-6"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div><div class="hidden-lg hidden-md hidden-sm col-xs-12"><a href="{{link}}"><img class="image" src="{{image}}"/></a></div>'
 
 
-    });
-    feed.run();
+  });
+  feed.run();
 });;
